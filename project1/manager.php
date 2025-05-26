48caedd576bf51afe57468590ad59b293a89b3d0
Alkel108 <106034458@student.swin.edu.au>
2025-05-26 15:51:11 +1000

    manager.php working

--- project1/manager.php
+++ project1/manager.php
@@ -2,15 +2,20 @@
 require_once('settings.php');
 $conn = mysqli_connect($host, $user, $pwd, $sql_db);
 
-function displayjobs($conn, $refnumber) {
+function displaydata($conn, $filter_value, $table, $delete) {
     // Escape the input
-    $job_ref = mysqli_real_escape_string($conn, $refnumber);
+    $escaped_filter_value = mysqli_real_escape_string($conn, $filter_value);
 
-    // Query with filter
-    $query = "SELECT * FROM eoi WHERE JobReferenceNumber = '$job_ref'";
+    if ( $delete){
+        $query = "DELETE FROM `eoi` WHERE $table = '$escaped_filter_value'";
+        echo "Deleted";
+    }
+    else{ $query = "SELECT * FROM `eoi` WHERE $table = '$escaped_filter_value'";}
+
     $result = mysqli_query($conn, $query);
-
-    if ($result) {
+    
+    if ($result && !$delete) {
+    
         // Loop through the filtered result set and print each row
         while ($row = mysqli_fetch_assoc($result)) {
             foreach ($row as $column => $value) {
@@ -18,21 +23,33 @@ function displayjobs($conn, $refnumber) {
             }
             echo '<hr>';
         }
-    } else {
-        echo "Error: " . mysqli_error($conn);
+    
     }
 }
 
+function change_eoi_status($conn, $eoi){}
+
 if($_SERVER['REQUEST_METHOD'] == 'POST')
 {
-    if (isset($_POST['G01'])) {displayjobs($conn, 'G01');}
-    if (isset($_POST['G02'])){displayjobs($conn, 'G02');}
-    if (isset($_POST['G03'])){displayjobs($conn, 'G03');}
-    if (isset($_POST['G04'])){displayjobs($conn, 'G04');}
-    if (isset($_POST['G05'])){displayjobs($conn, 'G05');}
-    if (isset($_POST['G06'])){displayjobs($conn, 'G06');}
-    if (isset($_POST['G07'])){displayjobs($conn, 'G07');}
 
+    // list by job
+    if (isset($_POST['G01'])) {displaydata($conn, 'G01', 'JobReferenceNumber', FALSE);}
+    if (isset($_POST['G02'])){displaydata($conn, 'G02', 'JobReferenceNumber', FALSE);}
+    if (isset($_POST['G03'])){displaydata($conn, 'G03', 'JobReferenceNumber', FALSE);}
+    if (isset($_POST['G04'])){displaydata($conn, 'G04', 'JobReferenceNumber', FALSE);}
+    if (isset($_POST['G05'])){displaydata($conn, 'G05', 'JobReferenceNumber', FALSE);}
+    if (isset($_POST['G06'])){displaydata($conn, 'G06', 'JobReferenceNumber', FALSE);}
+    if (isset($_POST['G07'])){displaydata($conn, 'G07', 'JobReferenceNumber', FALSE);}
+
+    if (isset($_POST['G01d'])) {displaydata($conn, 'G01', 'JobReferenceNumber', TRUE);}
+    if (isset($_POST['G02d'])){displaydata($conn, 'G02', 'JobReferenceNumber', TRUE);}
+    if (isset($_POST['G03d'])){displaydata($conn, 'G03', 'JobReferenceNumber', TRUE);}
+    if (isset($_POST['G04d'])){displaydata($conn, 'G04', 'JobReferenceNumber', TRUE);}
+    if (isset($_POST['G05d'])){displaydata($conn, 'G05', 'JobReferenceNumber', TRUE);}
+    if (isset($_POST['G06d'])){displaydata($conn, 'G06', 'JobReferenceNumber', TRUE);}
+    if (isset($_POST['G07d'])){displaydata($conn, 'G07', 'JobReferenceNumber', TRUE);}
+
+    // list all
     if (isset($_POST['listall'])) 
     {
        $query = "SELECT * FROM `eoi`";     // print all:
@@ -46,7 +63,31 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
             echo '<hr>';
         }
     }
-    else{echo 'error';}
+    if (isset($_POST['lastname'])){displaydata($conn, 'firstname', 'FirstName', FALSE);}
+
+
+
+    if (isset($_POST['change_status'])) {
+    $eoi_id = mysqli_real_escape_string($conn, $_POST['eoi_id']);
+    $new_status = mysqli_real_escape_string($conn, $_POST['new_status']);
+
+    // Example: use EOI ID as the identifier
+    $query = "UPDATE `eoi` SET `Status` = '$new_status' WHERE `EOInumber` = '$eoi_id'";
+
+    $result = mysqli_query($conn, $query);
+
+    if ($result) {
+        if (mysqli_affected_rows($conn) > 0) {
+            echo "EOI status updated successfully for EOI ID <strong>$eoi_id</strong>.<hr>";
+        } else {
+            echo "No EOI found";
+        }
+    } else {
+        echo "Error updating status: " . mysqli_error($conn);
+    }
+}
+
+
 }
 ?>
 
@@ -70,6 +111,39 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
         <label> <input type="checkbox" name="G07"> GO7</label>
         <label> <input type="submit" name="submitt" value="Submit"  /> </label>
     </fieldset>
+    <fieldset>
+    List from: 
+    <label> First name: <input type="textbox" name="firstname"> </label>
+    <label> Last name: <input type="textbox" name="lastname"> </label> 
+    <label> <input type="submit" name="submitt" value="Submit"  /> </label>
+
+    </fieldset>
+
+    <fieldset>
+        Delete from: 
+        <label> <input type="checkbox" name="G01d"> GO1</label>
+        <label> <input type="checkbox" name="G02d"> GO2</label>
+        <label> <input type="checkbox" name="G03d"> GO3</label>
+        <label> <input type="checkbox" name="G04d"> GO4</label>
+        <label> <input type="checkbox" name="G05d"> GO5</label>
+        <label> <input type="checkbox" name="G06d"> GO6</label>
+        <label> <input type="checkbox" name="G07d"> GO7</label>
+        <label> <input type="submit" name="Delete" value="Delete"  /> </label>
+    </fieldset>
+    <fieldset>
+    <legend>Change EOI Status</legend>
+    <label>EOI ID: <input type="text" name="eoi_id"></label><br>
+    <label>New Status: 
+        <select name="new_status">
+            <option value="New">New</option>
+            <option value="Current">Current</option>
+            <option value="Final">Final</option>
+        </select>
+    </label><br>
+    <input type="submit" name="change_status" value="Update Status">
+</fieldset>
+
+
 </form>
 </p>
 </html>
