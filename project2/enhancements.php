40e445fb6979157f15b0069fe42c8e578e970122
Alkel108 <106034458@student.swin.edu.au>
2025-05-28 19:56:57 +1000

    new changes

--- project2/enhancements.php
+++ project2/enhancements.php
@@ -7,7 +7,7 @@
 <p> The first enhancment we have added is the ability for the manager to select the order from which the eoi data is displayed </p>
 
 <p> The second enhancment is the log in page for the manager. The page requires the correct log in from the user that matches the usernmae password in the 'users' table in the database </p>
-
+<p> the manager can log in with username: 'admin' and password: 'password' </p>
 </main>
 <?php include 'footer.inc'; ?>    
 </html>
